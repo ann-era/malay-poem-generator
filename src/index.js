e87a0b90e0719e9fcca9385af2da5ba436f41a43
ann-era <148546178+ann-era@users.noreply.github.com>
2025-03-13 19:00:12 +0800

    Integrated SheCodes AI API for poem writing

--- src/index.js
+++ src/index.js
@@ -20,16 +20,34 @@ function generatePoem(event) {
     let poemBox = '<div class="poem" id="poem"></div>';
     main.insertAdjacentHTML("beforeend", poemBox);
   }
-  let writePoem = new Typewriter("#poem", {
+  new Typewriter("#poem", {
     strings: "Whipping up your poem...<br />Hang tight, creativity at work!",
     autoStart: true,
     cursor: null,
     delay: 50,
   });
-  writePoem
-    .deleteAll(25)
-    .typeString("Poem Line 1<br>Poem Line 2<br>Poem Line 3")
-    .start();
+
+  let keywords = document.querySelector("#topic-input").value;
+  let prompt = `In just 4 lines, generate a poem in malay about ${keywords}`;
+  let context = `You are a poetic AI assistant, skilled in crafting 4-line Malay poems, deeply attuned to nature, life, and the inner thoughts of humanity. Each line should be separated by <br />. You will sign your work with '<strong>- SheCodes AI</strong>'`;
+  let apiKey = "93aa3f2e40748o3cacebbc6bct7b0422";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(apiUrl);
+  axios
+    .get(apiUrl)
+    .then(writePoem)
+    .catch((error) => {
+      console.error("Error fetching poem: ", error);
+    });
+}
+
+function writePoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
 }
 
 let themeButton = document.querySelector("#theme");
