f07e5adf50c82a486572017e4c764d1abe6bf71a
ann-era <148546178+ann-era@users.noreply.github.com>
2025-03-13 20:00:43 +0800

    Fix dark theme bug
    Bug description: Example text is missing in dark theme

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ function toggleTheme() {
   let themeButton = document.querySelector("#theme");
   let body = document.querySelector("body");
   let container = document.querySelector("#container");
+  let smallNote = document.querySelector("small");
   if (themeButton.innerHTML == "☀️") {
     body.classList.add("dark-theme");
     container.classList.add("dark-theme");
+    smallNote.classList.add("dark-theme");
     themeButton.innerHTML = "🌙";
   } else {
     body.classList.remove("dark-theme");
     container.classList.remove("dark-theme");
+    smallNote.classList.remove("dark-theme");
     themeButton.innerHTML = "☀️";
   }
 }
