d8964893d0ce3cd9e4b533ee57f7a70f86e49987
ann-era <148546178+ann-era@users.noreply.github.com>
2025-03-14 15:36:45 +0800

    Implemented new functionality
    1. User able to set the desired emotional tone
    2. User able to copy poem to clipboard

--- src/index.js
+++ src/index.js
@@ -19,15 +19,21 @@ function toggleTheme() {
 function generatePoem(event) {
   event.preventDefault();
   let poemBox = document.querySelector("#poem");
+  let poemText = document.querySelector("#poem-text");
+  let copyButton = document.querySelector("#copy-btn");
   let keywords = document.querySelector("#topic-input").value;
-  let prompt = `In just 4 lines, generate a poem in malay about ${keywords}`;
+  let tone = document.querySelector("#tone-input").value;
+  let prompt = `In just 4 lines, generate a poem in malay about ${keywords} in a ${tone} tone`;
   let context = `You are a poetic AI assistant, skilled in crafting 4-line Malay poems, deeply attuned to nature, life, and the inner thoughts of humanity. Each line should be separated by <br />. You will sign your work with '<strong>- SheCodes AI</strong>'`;
   let apiKey = "93aa3f2e40748o3cacebbc6bct7b0422";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  if (poemBox.classList.contains("hidden")) {
-    poemBox.classList.remove("hidden");
+  if (poemBox.classList.contains("hidden-poem")) {
+    poemBox.classList.remove("hidden-poem");
   }
-  poemBox.innerHTML = `<span class="blinking-text">⏳ Crafting a poem inspired by ${keywords}..</span>`;
+  if (!copyButton.classList.contains("hidden-copy")) {
+    copyButton.classList.add("hidden-copy");
+  }
+  poemText.innerHTML = `<span class="blinking-text">⏳ Crafting a poem inspired by ${keywords} in a ${tone} tone..</span>`;
   axios
     .get(apiUrl)
     .then(writePoem)
@@ -37,12 +43,30 @@ function generatePoem(event) {
 }
 
 function writePoem(response) {
-  new Typewriter("#poem", {
+  let copyButton = document.querySelector("#copy-btn");
+  new Typewriter("#poem-text", {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 50,
   });
+  if (copyButton.classList.contains("hidden-copy")) {
+    copyButton.classList.remove("hidden-copy");
+  }
+  copyButton.addEventListener("click", copyPoem);
+}
+
+function copyPoem() {
+  let poemText = document.querySelector("#poem-text");
+  navigator.clipboard
+    .writeText(poemText.innerText)
+    .then(() => {
+      alert("Poem copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+      alert("Failed to copy poem. Please try again.");
+    });
 }
 
 let themeButton = document.querySelector("#theme");
